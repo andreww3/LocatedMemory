4c70cc533e77abfdd85829c612637e0cf2229696
Andrew Wang <45743645+andreww3@users.noreply.github.com>
2023-05-11 18:07:38 +1000

    Countdown timer
    Countdown timer with loop functionality based on timer

--- freerecall/experiment.js
+++ freerecall/experiment.js
@@ -1,8 +1,29 @@
-const jsPsych = initJsPsych();
+const jsPsych = initJsPsych({
+  display_element: 'jspsych-target',
+});
+
+const total_minutes = 1;
+const total_time = total_minutes * 60000 + 1000;
+var start_time;
+var timer_ticks;
 
 // INSTRUCTIONS =========================================================
 
 // TRIAL ================================================================
+
+var timer_start = {
+  type: jsPsychCallFunction,
+  func: () => {
+    document.querySelector('#countdown-timer-div').style.display = 'block';
+    start_time = Date.now();
+    timer_ticks = setInterval(() => {
+      var time_remaining = (total_time - (Date.now() - start_time)) / 1000;
+      var time_remaining_minutes = Math.max(Math.floor(time_remaining / 60), 0);
+      var time_remaining_seconds = Math.max(Math.floor(time_remaining % 60), 0);
+      document.getElementById("countdown-timer").innerHTML = `${time_remaining_minutes.toString().padStart(2, '0')}:${time_remaining_seconds.toString().padStart(2, '0')}`;
+    }, 100)
+  }
+}
 
 var textbox = {
   type: jsPsychMolFreeRecall,
@@ -12,15 +33,26 @@ var textbox = {
 
 var freeRecallTimeline = {
   timeline: [textbox],
-  loop_function: function(data) {
-    return true;
+  loop_function: () => {
+    if(Date.now() - start_time < total_time){
+      return true;
+    } else {
+      return false;
+    }
   }
 };
+
+
+
+
+//clearInterval(timer_ticks);
+//document.querySelector('#countdown-timer-div').style.display = 'none';
 
 // TIMELINE =============================================================
 
 var timeline = [];
 
+timeline.push(timer_start);
 timeline.push(freeRecallTimeline);
 
 jsPsych.run(timeline);
