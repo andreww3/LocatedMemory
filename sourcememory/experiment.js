0d9e860cd2f63078e53912931840c71cfb8e2308
Andrew Wang <45743645+andreww3@users.noreply.github.com>
2023-05-12 11:06:24 +1000

    Audio/Image
    - add test audio and image
    - test that audio/image works
    - add instructions to test

--- sourcememory/experiment.js
+++ sourcememory/experiment.js
@@ -8,7 +8,7 @@ const jsPsych = initJsPsych();
     // locus: a prompt (locus image, filepath)
     // data: an object containing the data to record for this stimulus (target/foil, locus, etc.)
 
-var stimuli = [];
+var stimuli = [{word: 'audio/1.wav', locus: 'img/rbb.jpg'}];
 
 // WELCOME ==============================================================
 
@@ -16,13 +16,25 @@ var stimuli = [];
 
 // INSTRUCTIONS =========================================================
 
+var instructions = {
+  type: jsPsychInstructions,
+  pages: [
+  'Welcome to the experiment. Click next to begin.',
+  'This is the second page of instructions.',
+  'This is the final page.'
+  ],
+  show_clickable_nav: true
+}
+
 // TRIAL ================================================================
 
 var trial = {
   type: jsPsychAudioKeyboardResponse,
   stimulus: jsPsych.timelineVariable('word'),
   choices: ['a', 'l'],
-  prompt: `<img src="${jsPsych.timelineVariable('locus')}">`,
+  prompt: () => {
+    return `<img src="${jsPsych.timelineVariable('locus')}">`
+  },
   data: jsPsych.timelineVariable('data'),
   response_ends_trial: true
 };
@@ -45,6 +57,7 @@ var preload = {
 
 var timeline = [];
 
+timeline.push(instructions);
 timeline.push(preload);
 timeline.push(trial_timeline);
 
