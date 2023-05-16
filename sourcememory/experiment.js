fba0d020817c91881d18a6b4e8ca0f9ceffc0e2c
Andrew Wang <45743645+andreww3@users.noreply.github.com>
2023-05-16 15:52:59 +1000

    Add special stimuli
    - Update stim.js for the special foil locus for each locus
    - Add special target/foil to stimuli object

--- sourcememory/experiment.js
+++ sourcememory/experiment.js
@@ -47,7 +47,8 @@ Object.keys(loci).forEach((i) => {
   }
 
   // add special words
-  // stimuli_data.push({word: special_words_current_locus.target[0], locus_orig: i, locus_exp});
+  stimuli_data.push({word: special_words_current_locus.target[0], locus_orig: i, locus_exp: i, type: "specialTarget"});
+  stimuli_data.push({word: special_words_current_locus.foil[0], locus_orig: i, locus_exp: loci_special_foils[i][0], type: "specialFoil"});
 });
 
 var stimuli = stimuli_data.map((data) => {
