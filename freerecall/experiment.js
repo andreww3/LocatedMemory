787c49d6cfc2e831dbd1d52c1ec79037ef736dd4
Andrew Wang <45743645+andreww3@users.noreply.github.com>
2023-05-31 17:54:12 +1000

    Update experiment.js
    - update time limit
    - add 'section' to textbox trials
    - practice: practice time limit, start practice timer function, textbox, timeline, end message, add to timeline

--- freerecall/experiment.js
+++ freerecall/experiment.js
@@ -6,8 +6,10 @@ const jsPsych = initJsPsych({
   }
 });
 
-const total_minutes = 15;
+const total_minutes = 20;
 const total_time = total_minutes * 60000 + 1000;
+const prac_minutes = 1;
+const prac_time = prac_minutes * 60000 + 1000;
 const num_last_messages = 5;
 var start_time;
 var timer_ticks;
@@ -27,6 +29,7 @@ var instructions = {
 var timer_start = {
   type: jsPsychCallFunction,
   func: () => {
+    // shows timer, records start time, calculates time remaining
     document.querySelector('#countdown-timer-div').style.display = 'block';
     start_time = Date.now();
     timer_ticks = setInterval(() => {
@@ -41,8 +44,9 @@ var timer_start = {
 var textbox = {
   type: jsPsychMolFreeRecall,
   prompt: "",
+  data: {section: 'test'},
   preamble: () => {
-    var previous_responses = jsPsych.data.get().last(num_last_messages).select('response').values;
+    var previous_responses = jsPsych.data.get().last(num_last_messages).filter({section: 'test'}).select('response').values;
     return `<div class="chat">
     <div>
     <p>${previous_responses.join("</p><p>")}</p>
@@ -62,7 +66,59 @@ var freeRecallTimeline = {
   }
 };
 
+// PRACTICE =============================================================
 
+var prac_timer_start = {
+  type: jsPsychCallFunction,
+  func: () => {
+    // shows timer, records start time, calculates time remaining
+    document.querySelector('#countdown-timer-div').style.display = 'block';
+    start_time = Date.now();
+    timer_ticks = setInterval(() => {
+      var time_remaining = (prac_time - (Date.now() - start_time)) / 1000;
+      var time_remaining_minutes = Math.max(Math.floor(time_remaining / 60), 0);
+      var time_remaining_seconds = Math.max(Math.floor(time_remaining % 60), 0);
+      document.getElementById("countdown-timer").innerHTML = `${time_remaining_minutes.toString().padStart(2, '0')}:${time_remaining_seconds.toString().padStart(2, '0')}`;
+    }, 100)
+  }
+}
+
+var practiceTextbox = {
+  type: jsPsychMolFreeRecall,
+  prompt: "",
+  data: {section: 'practice'},
+  preamble: () => {
+    var previous_responses = jsPsych.data.get().last(num_last_messages).select('response').values;
+    return `<div class="chat">
+    <div>
+    <p>${previous_responses.join("</p><p>")}</p>
+    </div>
+    </div>`
+  }
+};
+
+var practiceTimeline = {
+  timeline: [practiceTextbox],
+  loop_function: () => {
+    if(Date.now() - start_time < prac_time){
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+var pracEnd = {
+  type: jsPsychInstructions,
+  pages: ['You have finished the practice round. When instructed, click Next to continue.'],
+  show_clickable_nav: true,
+  allow_backward: false,
+  button_label_next: "Next",
+  on_start: () => {
+    clearInterval(timer_ticks);
+    document.querySelector('#countdown-timer-div').style.display = 'none';
+  }
+};
 
 // ENDSCREEN ============================================================
 
@@ -83,6 +139,9 @@ var debrief = {
 var timeline = [];
 
 timeline.push(instructions);
+timeline.push(prac_timer_start);
+timeline.push(practiceTimeline);
+timeline.push(pracEnd);
 timeline.push(timer_start);
 timeline.push(freeRecallTimeline);
 timeline.push(debrief);
